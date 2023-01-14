1bbf278c935c8ed9d8426c013647166f082f65f2
sisakat <isakhandy@gmail.com>
2023-01-14 16:47:33 +0100

    Add output before exporting

--- Main.pas
+++ Main.pas
@@ -10,15 +10,16 @@ var
   Exporter : TImageExporter;
   Painter  : TPainter      ;
 begin
-  Image    := TImage      .Create(256, 256 );
-  Exporter := TPPMExporter.Create           ;
-  Painter  := TPainter    .Create(Image    );
+  Image    := TImage      .Create(1920, 1080);
+  Exporter := TPPMExporter.Create            ;
+  Painter  := TPainter    .Create(Image     );
   try
     Image.Fill([0, 0, 0, 0]);
     Painter.DrawRect  (100, 100, 50, 50, [125, 125, 125, 255]);
     Painter.DrawCircle(100, 100, 50,     [180, 0  , 0  , 255]);
     Painter.DrawLine  (255, 255, 0  , 0, [0  , 255, 0  , 255]);
     Painter.DrawLine  (0  , 255, 128, 0, [0  , 0  , 255, 255]);
+    WriteLn('Exporting...');
     Exporter.Export(Image, 'output.ppm');
   finally
     FreeAndNil(Painter );
