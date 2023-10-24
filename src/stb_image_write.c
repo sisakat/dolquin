09419bfeacb066229774a464ab030a81abb84ecb
Stefan Isak <isakhandy@gmail.com>
2023-10-24 17:01:08 +0200

    Flip image vertically on write

--- src/stb_image_write.c
+++ src/stb_image_write.c
@@ -4,5 +4,6 @@
 
 int write_png(const char* filename, int width, int height, int comp, const char* data)
 {
+    stbi_flip_vertically_on_write(1);
     return stbi_write_png(filename, width, height, comp, (void*)data, width * comp);
 }
