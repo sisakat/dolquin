edc28d4d8ebdad79bd1645cb5490b24391ce0f05
Stefan Isak <isakhandy@gmail.com>
2023-10-25 18:22:51 +0200

    Undo flipping vertically

--- src/stb_image_write.c
+++ src/stb_image_write.c
@@ -4,6 +4,5 @@
 
 int write_png(const char* filename, int width, int height, int comp, const char* data)
 {
-    stbi_flip_vertically_on_write(1);
     return stbi_write_png(filename, width, height, comp, (void*)data, width * comp);
 }
